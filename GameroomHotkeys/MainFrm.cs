de0b2c326384338ccdd823e27e53fdc17aeecad8
put <44380176+put@users.noreply.github.com>
2019-02-16 00:24:33 +0100

    Added documentation for those who want to know what's going on!

--- GameroomHotkeys/MainFrm.cs
+++ GameroomHotkeys/MainFrm.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,6 +10,7 @@ namespace GameroomHotkeys
 {
     public partial class MainFrm : Form
     {
+        // Stuff I totally copied from a Google search, I don't know any of this off the top of my head
         [DllImport("user32.dll")]
         private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
         [DllImport("user32.dll")]
@@ -34,10 +31,11 @@ namespace GameroomHotkeys
             WinKey = 8
         }
 
+
         public const string FOUND_MSG = "Status: Klaar voor gebruik!";
-        public const string PROCESS_NAME = "Telegram";
-        public const string ENTER_KEY = "{ENTER}";
-        public const string ANSWER_A = "**A**";
+        public const string PROCESS_NAME = "Telegram"; // Obviously, the Telegram app's process name
+        public const string ENTER_KEY = "{ENTER}"; // This is how SendKeys wants to be told to 'press' enter
+        public const string ANSWER_A = "**A**"; // The ** makes the message bold in Telegram
         public const string ANSWER_B = "**B**";
         public const string GUESS_A = "**GOK:** A";
         public const string GUESS_B = "**GOK:** B";
@@ -48,66 +46,89 @@ namespace GameroomHotkeys
         public const string CREDITS_URL = "https://github.com/put";
         public const string CONTACT_URL = "https://t.me/mikatje";
 
+        // This will be the Telegram process once we found it
         public Process Telegram { get; set; }
-        public bool HotkeysSet { get; set; }
+        // This will be the stopwatch we use to make sure people don't spam answers
         public Stopwatch Throttler { get; set; }
 
+        // The form's constructor
         public MainFrm()
         {
             InitializeComponent();
-            HotkeysSet = false;
+            // Make new stopwatch
             Throttler = new Stopwatch();
+            // Call the function that will try to find the Telegram process
             FindTelegram();
         }
 
         private async Task FindTelegram()
         {
+            // We use this to keep our loop going for as long as we need
             bool telegramFound = false;
 
+            // A loop that runs until telegramFound is set to true (which would mean the process is found)
             while (!telegramFound)
             {
+                // Get a list of all processes running on the computer that match a certain process name (in our case, 'Telegram')
                 var processList = Process.GetProcessesByName(PROCESS_NAME);
 
+                // Check if any processes were found (by checking if the list has at least 1 process in it
                 if (processList.Length > 0)
                 {
+                    // Set this to true, so the loop will stop
                     telegramFound = true;
+                    // Save the Telegram process for later use, we simply assume it's the first process, if you run multiple processes called 'Telegram' this might cause trouble for you
                     Telegram = processList.First();
+                    // Next 4 lines are a bunch of visual stuff, they change texts and colors
                     StatusLbl.Text = FOUND_MSG;
                     StatusLbl.ForeColor = Color.Green;
                     StatusBx.BackColor = Color.Green;
                     Status2Bx.BackColor = Color.Green;
 
+                    // Register all hotkeys, which ones they are is probably fairly obvious. 1,2,3,4 are their identifiers
                     RegisterHotKey(Handle, 1, (int)KeyModifier.None, (int)Keys.F1);
                     RegisterHotKey(Handle, 2, (int)KeyModifier.None, (int)Keys.F2);
                     RegisterHotKey(Handle, 3, (int)KeyModifier.Shift, (int)Keys.F1);
                     RegisterHotKey(Handle, 4, (int)KeyModifier.Shift, (int)Keys.F2);
-
-                    HotkeysSet = true;
                 }
 
+                // finally, wait a second, this is because we don't want to check if the Telegram process exists thousands of times per second
                 await Task.Delay(1000);
             }
         }
 
+        // This is the function that does everything required to send an answer once a certain hotkey was pressed
         public void GiveAnswer(string message)
         {
+            // Bring the Telegram process to the front (focus on it)
             BringToFront(Telegram);
+            // Send the correct message + an enter to the currently focused window (which SHOULD be Telegram)
             SendKeys.SendWait(message + ENTER_KEY);
         }
 
+        // This is a function that triggers when a registered hotkey was pressed
         protected override void WndProc(ref Message m)
         {
+            // I wish I knew what this was for, this is part of what I copied from that Google search ;-)
             base.WndProc(ref m);
 
+            // Same as above, but I'm slightly more clueless about this one
             if (m.Msg == 0x0312)
             {
 
+                // This is the key that was pressed
                 Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+                // This is the key modifier that was pressed (shift, ctrl, etc)
                 KeyModifier modifier = (KeyModifier)((int)m.LParam & 0xFFFF);
+                // This is the identifier we gave to that particular hotkey
                 int id = m.WParam.ToInt32();
 
+                // Check if the stopwatch is either not running, or has been running for more than 3 seconds
+                // In both cases that means we allow the user to give an answer
                 if (!Throttler.IsRunning || Throttler.ElapsedMilliseconds > 3000)
                 {
+                    // Switch between supported hotkey identifiers
+                    // Inside of this, we really just make the correct call to the 'GiveAnswer' function, which does everything required to send a Telegram message
                     switch (id)
                     {
                         case 1:
@@ -124,22 +145,28 @@ namespace GameroomHotkeys
                             break;
                     }
 
+                    // Finally, we restart the stopwatch, so it will start at 0 seconds again. This is how we prevent spamming answers
                     Throttler.Restart();
                 }
             }
         }
 
+        // This function handles clicks on the 'GitHub' link, it just opens the website, really
         private void CreditsLbl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) 
             => Process.Start(CREDITS_URL);
 
+        // This function handles clicks on the 'Hulp' link, which displays a little message box containing (hopefully) helpful information
         private void HelpLbl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
             => MessageBox.Show(HELP_MESSAGE, HELP_TITLE, MessageBoxButtons.OK, MessageBoxIcon.Question);
 
+        // This function handles clicks on the 'Contact' link, it also just opens a website
         private void ContactLbl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
             => Process.Start(CONTACT_URL);
 
+        // This function triggers when the application is closed
         private void MainFrm_FormClosing(object sender, FormClosingEventArgs e)
         {
+            // When the application closes, we want to unregister all hotkeys again, which is what happens in the next 4 lines
             UnregisterHotKey(Handle, 1);
             UnregisterHotKey(Handle, 2);
             UnregisterHotKey(Handle, 3);
